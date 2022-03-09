64287fda2a1355c965f9289043d946113f1d3cff
Bernat Diaz <bernatdiaz04@gmail.com>
2022-03-09 10:14:23 +0100

    optional fname

--- app.py
+++ app.py
@@ -7,19 +7,11 @@ app = FastAPI()
 
 # Define what the app does
 @app.get("/greet")
-async def index(fname: str, lname: Optional[str] = None):
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
+async def index(fname: Optional[str] = None, lname: Optional[str] = None):
 
     if not fname and not lname:
         # If both first name and last name are missing, return an error
-        return jsonify({ "status" : "error" })
+        response = { "status" : "error" }
     elif fname and not lname:
         # If first name is present but last name is missing
         response = { "data" : f"Hello, {fname} !" }
