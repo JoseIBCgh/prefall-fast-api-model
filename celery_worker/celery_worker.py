c0e4302d18b850c6347ab506f146f19c21b6912c
Bernat Díaz <bernatdiaz94@gmail.com>
2023-10-20 09:09:54 -0400

    cambiar modelo

--- celery_worker/celery_worker.py
+++ celery_worker/celery_worker.py
@@ -24,10 +24,9 @@ import io
 
 import libraries
 
-with open('model_linear.pkl', 'rb') as fid:
-    clf = pickle.load(fid)
 
 modelo = pickle.load(open('modelo.pkl', 'rb'))
+
 
 @celery.task
 def create_order(name, quantity):
@@ -44,9 +43,6 @@ def create_order(name, quantity):
 
 @celery.task
 def predict(csv_content):
-    array = [1, 1, 1]
-    array = numpy.array(array).reshape(1,-1)
-    prediction = clf.predict_proba(array)[0]
 
     datos = pd.read_csv(io.StringIO(csv_content))
 
@@ -94,35 +90,21 @@ def predict(csv_content):
     #datos=datos.dropna()
     fases = datos.groupby('estado')
 
-    # Crea un dataframe vacío para almacenar las características
-    datos_final = pd.DataFrame(columns=['duracion_f1',
-                                            'lax_mean_f1', 'lay_mean_f1', 'laz_mean_f1',
-                                            'gx_mean_f1', 'gy_mean_f1', 'gz_mean_f1',
-                                            'mx_mean_f1', 'my_mean_f1', 'mz_mean_f1',
-                                            'qx_mean_f1', 'qy_mean_f1', 'qz_mean_f1','qw_mean_f1',
-                                            'duracion_f2',
-                                            'lax_mean_f2', 'lay_mean_f2', 'laz_mean_f2',
-                                            'gx_mean_f2', 'gy_mean_f2', 'gz_mean_f2',
-                                            'mx_mean_f2', 'my_mean_f2', 'mz_mean_f2',
-                                            'qx_mean_f2', 'qy_mean_f2', 'qz_mean_f2','qw_mean_f2',
-                                            'duracion_f3',
-                                            'lax_mean_f3', 'lay_mean_f3', 'laz_mean_f3',
-                                            'gx_mean_f3', 'gy_mean_f3', 'gz_mean_f3',
-                                            'mx_mean_f3', 'my_mean_f3', 'mz_mean_f3',
-                                            'qx_mean_f3', 'qy_mean_f3', 'qz_mean_f3','qw_mean_f3',
-                                            'duracion_f4',
-                                            'lax_mean_f4', 'lay_mean_f4', 'laz_mean_f4',
-                                            'gx_mean_f4', 'gy_mean_f4', 'gz_mean_f4',
-                                            'mx_mean_f4', 'my_mean_f4', 'mz_mean_f4',
-                                            'qx_mean_f4', 'qy_mean_f4', 'qz_mean_f4','qw_mean_f4',
-                                            ])
-
     # Itera sobre cada grupo de fases
-    i=1
-    for fase, grupo in fases:
+    fila = {}
+    for i, (fase, grupo) in enumerate(fases, start=1):
         # Calcula la duración de la fase
         duracion = grupo['TIME'].iloc[-1] - grupo['TIME'].iloc[0]
         duracion=len(grupo)/100
+
+        #acelerometro
+        ax_mean = grupo['ax'].mean()
+        ay_mean = grupo['ay'].mean()
+        az_mean = grupo['az'].mean()
+
+        ax_std = grupo['ax'].std()
+        ay_std = grupo['ay'].std()
+        az_std = grupo['az'].std()
 
         #CALCULAMOS VALORES MEDIOS DE SENSORES PARA CADA FASE DE MARCHA
         #Aceleracion lineal
@@ -148,54 +130,49 @@ def predict(csv_content):
 
         #CALCULAMOS DESVIACION TIPICA DE SENSROES PARA CADA FASE DE MARCHA
         #Aceleracion lineal
-        lax_mean = grupo['LACC_X'].std()
-        lay_mean = grupo['LACC_Y'].std()
-        laz_mean = grupo['LACC_Z'].std()
+        lax_std = grupo['LACC_X'].std()
+        lay_std = grupo['LACC_Y'].std()
+        laz_std = grupo['LACC_Z'].std()
 
         #giroscopio
-        gx_mean = grupo['GYR_X'].std()
-        gy_mean = grupo['GYR_Y'].std()
-        gz_mean = grupo['GYR_Z'].std()
+        gx_std = grupo['GYR_X'].std()
+        gy_std = grupo['GYR_Y'].std()
+        gz_std = grupo['GYR_Z'].std()
 
         #magnetometro
-        mx_mean = grupo['MAG_X'].std()
-        my_mean = grupo['MAG_Y'].std()
-        mz_mean = grupo['MAG_Z'].std()
+        mx_std = grupo['MAG_X'].std()
+        my_std = grupo['MAG_Y'].std()
+        mz_std = grupo['MAG_Z'].std()
 
         #quaterniones
-        qx_mean = grupo['QUAT_X'].std()
-        qy_mean = grupo['QUAT_Y'].std()
-        qz_mean = grupo['QUAT_Z'].std()
-        qw_mean = grupo['QUAT_Z'].std()
+        qx_std = grupo['QUAT_X'].std()
+        qy_std = grupo['QUAT_Y'].std()
+        qz_std = grupo['QUAT_Z'].std()
+        qw_std = grupo['QUAT_Z'].std()
 
 
-        # Agrega las características al dataframe de características
-        datos_final = datos_final.append({'duracion_f'+str(i): duracion, 
-                                                    'lax_mean_f'+str(i): lax_mean, 'lay_mean_f'+str(i): lay_mean, 'laz_mean_f'+str(i): laz_mean,
-                                                    'gx_mean_f'+str(i): gx_mean, 'gy_mean_f'+str(i): gy_mean, 'gz_mean_f'+str(i): gz_mean,
-                                                    'mx_mean_f'+str(i): mx_mean, 'my_mean_f'+str(i): my_mean, 'mz_mean_f'+str(i): mz_mean,
-                                                    'qx_mean_f'+str(i): qx_mean, 'qy_mean_f'+str(i): qy_mean, 'qz_mean_f'+str(i): qz_mean,'qw_mean_f'+str(i): qw_mean,
+        # Agrega las características al diccionario fila
+        fila.update({
+            'duracion_f'+str(i): duracion,
+            'ax_mean_f'+str(i): ax_mean, 'ay_mean_f'+str(i): ay_mean, 'az_mean_f'+str(i): az_mean,
+            'ax_std_f'+str(i): ax_std, 'ay_std_f'+str(i): ay_std, 'az_std_f'+str(i): az_std,
+            'lax_mean_f'+str(i): lax_mean, 'lay_mean_f'+str(i): lay_mean, 'laz_mean_f'+str(i): laz_mean,
+            'lax_std_f'+str(i): lax_std, 'lay_std_f'+str(i): lay_std, 'laz_std_f'+str(i): laz_std,
+            'gx_mean_f'+str(i): gx_mean, 'gy_mean_f'+str(i): gy_mean, 'gz_mean_f'+str(i): gz_mean,
+            'gx_std_f'+str(i): gx_std, 'gy_std_f'+str(i): gy_std, 'gz_std_f'+str(i): gz_std,
+            'mx_mean_f'+str(i): mx_mean, 'my_mean_f'+str(i): my_mean, 'mz_mean_f'+str(i): mz_mean,
+            'mx_std_f'+str(i): mx_std, 'my_std_f'+str(i): my_std, 'mz_std_f'+str(i): mz_std,
+        })
 
-                                                    }, ignore_index=True)
+    df_fila = pd.DataFrame([fila])
+    prediccion=modelo.predict_proba([df_fila.iloc[0].tolist()])[0][1]
 
-        i=i+1
-
-
-
-    #Combinamos los resultados
-    datos_final = pd.concat([datos_final.iloc[0], datos_final.iloc[1], datos_final.iloc[2],datos_final.iloc[3]]).dropna()
-
-    # Crear un nuevo dataframe con una sola fila y las columnas resultantes
-    datos_final = pd.DataFrame([datos_final.tolist()], columns=datos_final.index)
-
-    #Predecimos la probabilidad de caida con el modelo
-    prediccion=modelo.predict_proba([datos_final.iloc[0].tolist()])[0][1]
     texto="Probabildiad de caida del paciente : "+str(prediccion)
 
     celery_log.info(f"Prediction Complete!")
     celery_log.info(texto)
     return {"model_id": model_id(),"message": "Prediction complete", 
-    "probability": prediccion, "datos": datos_final.to_dict(orient='records')}
+    "probability": prediccion, "datos": fila}
 
 def coef():
     coef_avg = 0
